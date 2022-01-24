67e171c02d6bbc1661f9581b0639dc932e74c3b5
Severin <insmer@gmail.com>
2022-01-24 22:39:49 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Dmitriy");
+        System.out.println("28.06.2022");
+    }
+}
